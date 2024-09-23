66ae4a6dec5cc790595e898d968f33d28173e17b
Pedro Gomes <108994150+pedro-ggomes@users.noreply.github.com>
2024-09-23 08:28:49 -0300

    added the js logic to grab all the sidebar links for scrapy, also added the .txt coverted output file

---
+++ scrape_jitterbit/scrape_jitterbit/get_urls.js
@@ -0,0 +1,22 @@
+// paste this line by line to get all links from the navbar
+// then feed it to the spider's start_urls prop
+
+var elements = document.getElementsByClassName("md-nav__link localLink");
+
+var elementsArr = Array.prototype.slice.call(elements);
+
+var links = elementsArr.map((elem) => {
+  return "https://docs.jitterbit.com/" + elem.getAttribute("href");
+});
+
+var linksString = JSON.stringify(links);
+
+var blob = new Blob([linksString], { type: "text/plain" });
+
+var link = document.createElement("a");
+
+link.href = URL.createObjectURL(blob);
+
+link.download = "alldocslinks.txt";
+
+link.click();
